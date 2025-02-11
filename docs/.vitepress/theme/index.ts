45a7b69e031c6af8af53a6f912dca4b84ca9eecc
Zong Fan <wkndnite@126.com>
2025-02-11 14:38:31 +0800

    fix(docs/.vitepress/theme): :bug: 清理无用注释和代码

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,11 +1,6 @@
 import BlogTheme from '@sugarat/theme';
 import Fireworks from './components/Fireworks.vue';
-
-// 自定义样式重载
 import './style.scss';
-
-// 自定义主题色
-// import './user-theme.css'
 import { h } from 'vue';
 
 export default {
@@ -13,13 +8,12 @@ export default {
   Layout: (props: any) => {
     if (BlogTheme.Layout) {
       return h(BlogTheme.Layout, props, {
-        'layout-top': () => h(Fireworks), // 在 layout-top 插槽中渲染 Fireworks 组件
+        'layout-top': () => h(Fireworks),
       });
     }
-    // 如果 BlogTheme.Layout 是 undefined，可以选择不渲染任何内容或其他处理
-    return null; // 或者其他逻辑
+    return null;
   },
   enhanceApp({ app, router, siteData }) {
-    app.component('Fireworks', Fireworks); // 注册 Fireworks 组件
+    app.component('Fireworks', Fireworks);
   },
 };
