11eb08766f675825838fcdaf33e927c2929720d0
Zong Fan <wkndnite@126.com>
2025-02-11 19:02:28 +0800

    fix(theme): :bug: 注释掉分享按钮组件相关代码并更新vitepress版本

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,23 +2,24 @@ import BlogTheme from '@sugarat/theme';
 import Fireworks from './components/Fireworks.vue';
 import './style.scss';
 import { h } from 'vue';
-import { ShareButton } from '@theojs/lumen';
+// import { ShareButton } from '@theojs/lumen';
 
 export default {
+  
   ...BlogTheme,
   Layout: (props: any) => {
     if (BlogTheme.Layout) {
       return h(BlogTheme.Layout, props, {
         'layout-top': () => h(Fireworks),
-        'nav-bar-content-after': () => h(ShareButton,{
-          copiedText: '链接已复制',
-        }),
+        // 'nav-bar-content-after': () => h(ShareButton,{
+        //   copiedText: '链接已复制',
+        // }),
       });
     }
     return null;
   },
   enhanceApp({ app, router, siteData }) {
     app.component('Fireworks', Fireworks);
-    app.component('ShareButton', ShareButton);
+    // app.component('ShareButton', ShareButton);
   },
 };
