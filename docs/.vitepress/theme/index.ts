a543a2d5313192fff34064ea0b343c8d51879dd6
Zong Fan <wkndnite@126.com>
2025-02-11 17:52:59 +0800

    feat(theme): :sparkles: 添加分享按钮组件

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,6 +2,7 @@ import BlogTheme from '@sugarat/theme';
 import Fireworks from './components/Fireworks.vue';
 import './style.scss';
 import { h } from 'vue';
+import { ShareButton } from '@theojs/lumen';
 
 export default {
   ...BlogTheme,
@@ -9,11 +10,15 @@ export default {
     if (BlogTheme.Layout) {
       return h(BlogTheme.Layout, props, {
         'layout-top': () => h(Fireworks),
+        'nav-bar-content-after': () => h(ShareButton,{
+          copiedText: '链接已复制',
+        }),
       });
     }
     return null;
   },
   enhanceApp({ app, router, siteData }) {
     app.component('Fireworks', Fireworks);
+    app.component('ShareButton', ShareButton);
   },
 };
