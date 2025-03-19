ba8cf4da58be74fcbde25176ae53f2c0890d7048
Zong Fan <wkndnite@126.com>
2025-03-19 14:08:32 +0800

    fix: :bug: 修复阻尼问题

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import BlogTheme from '@sugarat/theme';
 import { ShareButton } from '@theojs/lumen';
-import { useData } from 'vitepress';
+import Lenis from 'lenis';
+import { inBrowser, useData } from 'vitepress';
 import { h } from 'vue';
 import Fireworks from './components/Fireworks.vue';
 import MNavLinks from './components/MNavLinks.vue';
@@ -35,5 +36,23 @@ export default {
     ctx.app.component('Fireworks', Fireworks);
     ctx.app.component('ShareButton', ShareButton);
     ctx.app.component('VideoPlayer', VideoPlayer);
+    if (inBrowser) {
+      const lenis = new Lenis();
+      const lenisInit = (time: number) => {
+        lenis.raf(time);
+        requestAnimationFrame(lenisInit);
+      };
+      lenis.on('scroll', () => {
+        if (
+          window.scrollY + window.innerHeight >=
+          document.documentElement.scrollHeight
+        ) {
+          lenis.stop();
+          lenis.start();
+        }
+      });
+
+      requestAnimationFrame(lenisInit);
+    }
   }
 };
