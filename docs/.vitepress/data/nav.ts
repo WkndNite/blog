38c4aadb0abc36bfcc54e4c2037d0222a6d7406a
Zong Fan <wkndnite@126.com>
2025-03-30 12:50:14 +0800

    feat(docs/Interview/Experience): :sparkles: add experiences from 327 to 328
    some lint modify

---
+++ docs/.vitepress/data/nav.ts
@@ -0,0 +1,120 @@
+export const nav = [
+  {
+    text: '站点工具',
+    items: [
+      { text: '导航集锦', link: '/Nav' },
+      { text: '流量管理', link: 'https://v6.51.la' }
+    ]
+  },
+  {
+    text: 'CS 基础',
+    items: [
+      {
+        text: 'LeetCode Hot 100',
+        link: '/CS/LeetCode-100'
+      },
+      {
+        text: '数据结构与算法',
+        link: '/CS/DSA'
+      },
+      {
+        text: '代码随想录',
+        link: '/CS/programmercarl'
+      }
+    ]
+  },
+  {
+    text: '前端漫游',
+    items: [
+      { text: 'CSS', link: '/Frontend/CSS' },
+      {
+        text: 'JavaScript',
+        link: '/Frontend/JavaScript'
+      },
+      {
+        text: 'ESnext',
+        link: '/Frontend/ESnext'
+      },
+      {
+        text: 'TypeScript',
+        link: '/Frontend/TypeScript'
+      },
+      {
+        text: 'Webpack',
+        link: '/Frontend/Webpack'
+      },
+      {
+        text: '工程化',
+        link: '/Frontend/Engineering'
+      },
+      {
+        text: 'Vue',
+        link: '/Frontend/Vue'
+      },
+      {
+        text: 'React',
+        link: '/Frontend/React'
+      },
+      {
+        text: '浏览器',
+        link: '/Frontend/Browser'
+      },
+      {
+        text: 'Debug',
+        link: '/Frontend/Projects'
+      },
+      {
+        text: '杂谈',
+        link: '/Frontend/Others/some'
+      }
+    ]
+  },
+  {
+    text: '软技能',
+    items: [
+      {
+        text: 'Git',
+        link: '/DevTips/Git'
+      },
+      {
+        text: 'Docker',
+        link: '/DevTips/Docker'
+      },
+      {
+        text: '开发范式',
+        link: '/DevTips/Design'
+      },
+      {
+        text: '开发工具',
+        link: '/DevTips/Others'
+      }
+    ]
+  },
+  {
+    text: '校招备战',
+    items: [
+      {
+        text: '网络',
+        link: '/Interview/Network'
+      },
+      {
+        text: 'CSS',
+        link: '/Interview/CSS'
+      },
+      {
+        text: 'JavaScript',
+        link: '/Interview/JavaScript'
+      },
+      {
+        text: '手撕代码',
+        link: '/Interview/diy'
+      },
+      {
+        text: '笔面经验',
+        link: '/Interview/Experience'
+      }
+    ]
+  },
+  { text: '个人作品', link: '/Works' },
+  { text: '生活随笔', link: '/Life/' }
+];
